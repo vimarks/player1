ed13c91d6ea2c59eb744cb43d6b31e15884b85b8
david cochran <about.trout@gmail.com>
2020-03-02 19:28:05 -0800

    Add laser beams that move slowly

--- app.js
+++ app.js
@@ -3,11 +3,16 @@ Stage(function(stage) {
   let twoPi = 2 * Math.PI
   let activeKeys = {}
   let KEY_NAMES = {
+    32: 'space',
     37: 'right',
     38: 'up',
     39: 'left',
     40: 'down',
   }
+
+  let bullets = []
+  let reloading = false
+  let RELOAD_TIME = 750
 
   // Create an image and append it to stage
   let rotation = 0
@@ -78,9 +83,52 @@ Stage(function(stage) {
     shuttle.pin({
       rotation: rotation,
       offsetX: offsetX,
-      offsetY, offsetY,
+      offsetY: offsetY,
     })
+
+    if (activeKeys.space && !reloading) {
+      bullets.push(new Bullet(offsetX, offsetY, rotation, velocityX, velocityY))
+      reloading = true
+      setTimeout(function() { reloading = false }, RELOAD_TIME)
+    }
+
+    for (bullet of bullets) {
+      bullet.tick(dt)
+    }
   })
+
+  function Bullet(x, y, rotation, velocityX, velocityY) {
+    let bullet = Stage.image('bullet')
+    bullet.appendTo(stage)
+
+    this.img = bullet
+    this.x = x
+    this.y = y
+    this.rotation = rotation
+    this.velocityX = velocityX + 25
+    this.velocityY = velocityY + 25
+    return this
+  }
+
+  Bullet.prototype.tick = function(dt) {
+    this.x += Math.sin(this.rotation) * (this.velocityX / dt)
+    this.y -= Math.cos(this.rotation) * (this.velocityY / dt)
+
+    let halfWidth = width / 2
+    let halfHeight = height / 2
+    if (this.x >= halfWidth || this.x <= -halfWidth) {
+      this.x = -this.x
+    }
+    if (this.y >= halfHeight || this.y <= -halfHeight) {
+      this.y = -this.y
+    }
+    this.img.pin({
+      offsetX: this.x,
+      offsetY: this.y,
+      align: 0.5,
+      rotation: this.rotation
+    })
+  }
 
 })
 
@@ -89,5 +137,12 @@ Stage({
   image : '/static/shuttle.png',
   textures : {
     shuttle : { x : 0, y : 0, width : 52, height : 52 }
+  }
+})
+
+Stage({
+  image: '/static/laser-blue.png',
+  textures: {
+    bullet: { x: 0, y: 0, width: 15, height: 30 } 
   }
 })
