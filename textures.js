ecf01afc95c547c88e48ad1cedafec49963e2600
david cochran <about.trout@gmail.com>
2020-03-07 12:48:03 -0800

    Add rocks
    Rocks are randomly placed, have a random but fixed velocity, and wrap
    around the screen like bullets and ships.

--- textures.js
+++ textures.js
@@ -1,4 +1,3 @@
-
 Stage({
   image : '/static/shuttle.png',
   textures : {
@@ -10,5 +9,12 @@ Stage({
   image: '/static/laser-blue.png',
   textures: {
     bullet: { x: 0, y: 0, width: 15, height: 30 }
+  }
+})
+
+Stage({
+  image: '/static/asteroid-brown.png',
+  textures: {
+    rock: { x: 0, y: 0, width: 64, height: 64 }
   }
 })
