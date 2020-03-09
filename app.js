23238cef5159318910fc190d062b6a0fa2873216
Ian Good <icgood@gmail.com>
2020-03-09 21:48:01 +0000

    Rework the controls and math

--- app.js
+++ app.js
@@ -1,35 +1,4 @@
-import { State } from "./lib/state.js";
+import init from "./lib/init.js";
 
-/**
- * Binds keyboard keys, using the built-in `onkeyup` and `onkeydown`
- * DOM events.
- */
-function bindKeys() {
-  let activeKeys = {};
-  let KEY_NAMES = {
-    32: "space",
-    37: "right",
-    38: "up",
-    39: "left",
-    40: "down"
-  };
-
-  document.onkeydown = function(evt) {
-    activeKeys[KEY_NAMES[evt.keyCode]] = true;
-  };
-
-  document.onkeyup = function(evt) {
-    activeKeys[KEY_NAMES[evt.keyCode]] = false;
-  };
-
-  return activeKeys;
-}
-
-/**
- * Initializes the game state and binds the tick function to run
- * every time the screen is rendered.
- */
-Stage(function(stage) {
-  let state = new State(bindKeys());
-  state.start(stage);
-});
+// Initialize the application.
+Stage(init);
