fbded45da291925ca0e58eeda610b99824476602
Vincent Marks <violateacher123@gmail.com>
2020-03-09 13:57:15 -0500

    ran linter/added comment

--- textures.js
+++ textures.js
@@ -1,14 +1,13 @@
-
 Stage({
-  image : '/static/shuttle.png',
-  textures : {
-    shuttle : { x : 0, y : 0, width : 52, height : 52 }
+  image: "/static/shuttle.png",
+  textures: {
+    shuttle: { x: 0, y: 0, width: 52, height: 52 }
   }
-})
+});
 
 Stage({
-  image: '/static/laser-blue.png',
+  image: "/static/laser-blue.png",
   textures: {
     bullet: { x: 0, y: 0, width: 15, height: 30 }
   }
-})
+});
