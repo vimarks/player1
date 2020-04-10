48df8b23effb36e5c7957b32f055ed8dd02ce7ba
Vincent Marks <violateacher123@gmail.com>
2020-04-10 10:43:18 -0500

    removed sound import to crystalmaker.js

--- src/server/crystalmaker.js
+++ src/server/crystalmaker.js
@@ -1,7 +1,6 @@
 import constants from '../constants.js'
 import { Event } from '../event.js'
 import { randRange } from '../rand.js'
-import sounds from '../client/sounds.js'
 import { elapsed } from '../time.js'
 
 export class CrystalMaker {
